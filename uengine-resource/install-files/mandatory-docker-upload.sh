88c8681a8f18a4709c178e609f331887e64ad8cd
darkgodarkgo@gmail.com <darkgodarkgo@gmail.com>
2018-03-22 11:40:46 +0900

    #73 Snapshot create,list UI

--- uengine-resource/install-files/mandatory-docker-upload.sh
+++ uengine-resource/install-files/mandatory-docker-upload.sh
@@ -4,87 +4,87 @@
 # Untag this, if you need sudo
 # sudo su
 
-docker pull mesosphere/marathon-lb:v1.11.2
-docker tag mesosphere/marathon-lb:v1.11.2 sppark/marathon-lb:v1.11.2
-docker push sppark/marathon-lb:v1.11.2
-docker rmi sppark/marathon-lb:v1.11.2
-docker rmi mesosphere/marathon-lb:v1.11.2
+docker pull sppark/uengine-lb:v1
+docker tag sppark/uengine-lb:v1 darkgodarkgo/uengine-lb:v1
+docker push darkgodarkgo/uengine-lb:v1
+docker rmi darkgodarkgo/uengine-lb:v1
+docker rmi sppark/uengine-lb:v1
 
 
 docker pull mysql:5.7
-docker tag mysql:5.7 sppark/mysql:5.7
-docker push sppark/mysql:5.7
-docker rmi sppark/mysql:5.7
+docker tag mysql:5.7 darkgodarkgo/mysql:5.7
+docker push darkgodarkgo/mysql:5.7
+docker rmi darkgodarkgo/mysql:5.7
 docker rmi mysql:5.7
 
 
 docker pull docker:latest
-docker tag docker:latest sppark/docker:latest
-docker push sppark/docker:latest
-docker rmi sppark/docker:latest
+docker tag docker:latest darkgodarkgo/docker:latest
+docker push darkgodarkgo/docker:latest
+docker rmi darkgodarkgo/docker:latest
 docker rmi docker:latest
 
 
 docker pull node:latest
-docker tag node:latest sppark/node:latest
-docker push sppark/node:latest
-docker rmi sppark/node:latest
+docker tag node:latest darkgodarkgo/node:latest
+docker push darkgodarkgo/node:latest
+docker rmi darkgodarkgo/node:latest
 docker rmi node:latest
 
 
 docker pull maven:3-jdk-8
-docker tag maven:3-jdk-8 sppark/maven:3-jdk-8
-docker push sppark/maven:3-jdk-8
-docker rmi sppark/maven:3-jdk-8
+docker tag maven:3-jdk-8 darkgodarkgo/maven:3-jdk-8
+docker push darkgodarkgo/maven:3-jdk-8
+docker rmi darkgodarkgo/maven:3-jdk-8
 docker rmi maven:3-jdk-8
 
 
 docker pull maven:3-jdk-7
-docker tag maven:3-jdk-7 sppark/maven:3-jdk-7
-docker push sppark/maven:3-jdk-7
-docker rmi sppark/maven:3-jdk-7
+docker tag maven:3-jdk-7 darkgodarkgo/maven:3-jdk-7
+docker push darkgodarkgo/maven:3-jdk-7
+docker rmi darkgodarkgo/maven:3-jdk-7
 docker rmi maven:3-jdk-7
 
 
 docker pull sppark/curl-jq:v1
-docker tag sppark/curl-jq:v1 sppark/curl-jq:v1
-docker push sppark/curl-jq:v1
-docker rmi sppark/curl-jq:v1
+docker tag sppark/curl-jq:v1 darkgodarkgo/curl-jq:v1
+docker push darkgodarkgo/curl-jq:v1
+docker rmi darkgodarkgo/curl-jq:v1
 docker rmi sppark/curl-jq:v1
 
 
 docker pull openjdk:8u111-jdk-alpine
-docker tag openjdk:8u111-jdk-alpine sppark/openjdk:8u111-jdk-alpine
-docker push sppark/openjdk:8u111-jdk-alpine
-docker rmi sppark/openjdk:8u111-jdk-alpine
+docker tag openjdk:8u111-jdk-alpine darkgodarkgo/openjdk:8u111-jdk-alpine
+docker push darkgodarkgo/openjdk:8u111-jdk-alpine
+docker rmi darkgodarkgo/openjdk:8u111-jdk-alpine
 docker rmi openjdk:8u111-jdk-alpine
 
 
 docker pull tomcat:7.0.84-jre7
-docker tag tomcat:7.0.84-jre7 sppark/tomcat:7.0.84-jre7
-docker push sppark/tomcat:7.0.84-jre7
-docker rmi sppark/tomcat:7.0.84-jre7
+docker tag tomcat:7.0.84-jre7 darkgodarkgo/tomcat:7.0.84-jre7
+docker push darkgodarkgo/tomcat:7.0.84-jre7
+docker rmi darkgodarkgo/tomcat:7.0.84-jre7
 docker rmi tomcat:7.0.84-jre7
 
 
 docker pull webratio/nodejs-http-server
-docker tag webratio/nodejs-http-server sppark/nodejs-http-server
-docker push sppark/nodejs-http-server
-docker rmi sppark/nodejs-http-server
+docker tag webratio/nodejs-http-server darkgodarkgo/nodejs-http-server
+docker push darkgodarkgo/nodejs-http-server
+docker rmi darkgodarkgo/nodejs-http-server
 docker rmi webratio/nodejs-http-server
 
 
 docker pull google/cadvisor:latest
-docker tag google/cadvisor:latest sppark/cadvisor:latest
-docker push sppark/cadvisor:latest
-docker rmi sppark/cadvisor:latest
+docker tag google/cadvisor:latest darkgodarkgo/cadvisor:latest
+docker push darkgodarkgo/cadvisor:latest
+docker rmi darkgodarkgo/cadvisor:latest
 docker rmi google/cadvisor:latest
 
 
 docker pull sonatype/nexus:2.14.6-02
-docker tag sonatype/nexus:2.14.6-02 sppark/nexus:2.14.6-02
-docker push sppark/nexus:2.14.6-02
-docker rmi sppark/nexus:2.14.6-02
+docker tag sonatype/nexus:2.14.6-02 darkgodarkgo/nexus:2.14.6-02
+docker push darkgodarkgo/nexus:2.14.6-02
+docker rmi darkgodarkgo/nexus:2.14.6-02
 docker rmi sonatype/nexus:2.14.6-02
 
 
