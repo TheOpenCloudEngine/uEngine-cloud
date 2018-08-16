525cbc7eecf5294d49acaec77cccd84d36cd0ab6
darkgodarkgo@gmail.com <darkgodarkgo@gmail.com>
2018-08-16 11:58:07 +0900

    #96 Logfilter work only in billing enable

--- uengine-cloud-zuul/src/main/java/org/uengine/zuul/post/LogFilter.java
+++ uengine-cloud-zuul/src/main/java/org/uengine/zuul/post/LogFilter.java
@@ -51,54 +51,56 @@ public class LogFilter extends ZuulFilter {
         BillingConfig billingConfig = ApplicationContextRegistry.getApplicationContext().getBean(BillingConfig.class);
         LogService logService = ApplicationContextRegistry.getApplicationContext().getBean(LogService.class);
 
-        meteringLog.setMethod(request.getMethod());
-        meteringLog.setRemoteAddr(request.getRemoteAddr());
-        meteringLog.setRequestURI(request.getRequestURI());
+        if(billingConfig.isEnable()){
+            meteringLog.setMethod(request.getMethod());
+            meteringLog.setRemoteAddr(request.getRemoteAddr());
+            meteringLog.setRequestURI(request.getRequestURI());
 
-        try {
-            meteringLog.setServiceId(tokenContext.getRoute().get("serviceId").toString());
-        } catch (Exception ex) {
+            try {
+                meteringLog.setServiceId(tokenContext.getRoute().get("serviceId").toString());
+            } catch (Exception ex) {
 
-        }
-        meteringLog.setResponseStatus(ctx.getResponseStatusCode());
-        List<UserSubscriptions.Subscription> subscriptions = tokenContext.getUserSubscriptions().getSubscriptions();
-
-        List<MeteringLog.Subscription> list = new ArrayList<>();
-        if(subscriptions != null){
-            for (int i = 0; i < subscriptions.size(); i++) {
-                MeteringLog.Subscription subscription = new MeteringLog.Subscription();
-                subscription.setId(subscriptions.get(i).getId());
-                subscription.setPlan(subscriptions.get(i).getPlan());
-                subscription.setProduct(subscriptions.get(i).getProduct());
-                list.add(subscription);
             }
-        }
-        meteringLog.setSubscriptions(list);
-        meteringLog.setClientKey(tokenContext.getClientKey());
-        meteringLog.setUserName(tokenContext.getUserName());
-        meteringLog.setAccountId(tokenContext.getUserSubscriptions().getAccountId());
-        meteringLog.setTimestamp(new Timestamp(new Date().getTime()));
-        meteringLog.setOrganizationId(billingConfig.getOrganization());
-        logService.addMeteringLog(meteringLog);
+            meteringLog.setResponseStatus(ctx.getResponseStatusCode());
+            List<UserSubscriptions.Subscription> subscriptions = tokenContext.getUserSubscriptions().getSubscriptions();
 
-        //Add usageLog
-        BillingContext.UsageLog usageLog = tokenContext.getBillingContext().getUsageLog();
-        if (usageLog != null) {
-            int statusCode = ctx.getResponseStatusCode();
-            Integer[] when = usageLog.getWhen();
-            //save if status code in when options
-            if (Arrays.asList(when).contains(statusCode)) {
-                UsageLog log = new UsageLog();
-                log.setUserName(tokenContext.getUserName());
-                log.setClientKey(tokenContext.getClientKey());
-                log.setPlan(usageLog.getPlan());
-                log.setSubscriptionId(usageLog.getSubscriptionId());
-                log.setAccountId(tokenContext.getUserSubscriptions().getAccountId());
-                log.setUnit(usageLog.getUnit());
-                log.setTimestamp(new Timestamp(new Date().getTime()));
-                log.setProduct(usageLog.getProduct());
-                log.setOrganizationId(billingConfig.getOrganization());
-                logService.addUsageLog(log);
+            List<MeteringLog.Subscription> list = new ArrayList<>();
+            if(subscriptions != null){
+                for (int i = 0; i < subscriptions.size(); i++) {
+                    MeteringLog.Subscription subscription = new MeteringLog.Subscription();
+                    subscription.setId(subscriptions.get(i).getId());
+                    subscription.setPlan(subscriptions.get(i).getPlan());
+                    subscription.setProduct(subscriptions.get(i).getProduct());
+                    list.add(subscription);
+                }
+            }
+            meteringLog.setSubscriptions(list);
+            meteringLog.setClientKey(tokenContext.getClientKey());
+            meteringLog.setUserName(tokenContext.getUserName());
+            meteringLog.setAccountId(tokenContext.getUserSubscriptions().getAccountId());
+            meteringLog.setTimestamp(new Timestamp(new Date().getTime()));
+            meteringLog.setOrganizationId(billingConfig.getOrganization());
+            logService.addMeteringLog(meteringLog);
+
+            //Add usageLog
+            BillingContext.UsageLog usageLog = tokenContext.getBillingContext().getUsageLog();
+            if (usageLog != null) {
+                int statusCode = ctx.getResponseStatusCode();
+                Integer[] when = usageLog.getWhen();
+                //save if status code in when options
+                if (Arrays.asList(when).contains(statusCode)) {
+                    UsageLog log = new UsageLog();
+                    log.setUserName(tokenContext.getUserName());
+                    log.setClientKey(tokenContext.getClientKey());
+                    log.setPlan(usageLog.getPlan());
+                    log.setSubscriptionId(usageLog.getSubscriptionId());
+                    log.setAccountId(tokenContext.getUserSubscriptions().getAccountId());
+                    log.setUnit(usageLog.getUnit());
+                    log.setTimestamp(new Timestamp(new Date().getTime()));
+                    log.setProduct(usageLog.getProduct());
+                    log.setOrganizationId(billingConfig.getOrganization());
+                    logService.addUsageLog(log);
+                }
             }
         }
 
